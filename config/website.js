79bfcc1c29fe60d20c9c727986f3fc0fb58ef050
Ritesh Patil <riteshsp2000@gmail.com>
2021-10-13 17:46:35 +0530

    chore: update gatsby config and add a content folder for managing content

---
+++ config/website.js
@@ -0,0 +1,31 @@
+/* eslint-disable max-len */
+
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
+  siteTitle: 'Slyde', // Navigation and Site Title
+  siteTitleAlt: 'Task Management App', // Alternative Site title for SEO
+  siteTitleShort: 'Slyde', // short_name for manifest
+  siteUrl: 'https://slyde.netlify.app', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: '/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
+  siteDescription: 'Manage all your tasks easily',
+  author: 'Ritesh Patil', // Author for schemaORGJSONLD
+  organization: 'Slyde',
+
+  siteFBAppID: '', // Facebook App ID
+  userTwitter: '@riteshsp2000', // Twitter Username
+  ogSiteName: 'Slyde', // Facebook Site Name
+  ogLanguage: 'en_UK',
+  googleAnalyticsID: '',
+
+  // Manifest and Progress color
+  themeColor: '#ffffff',
+  backgroundColor: '#161D28',
+
+  // Social component
+  twitterHandle: '@riteshsp2000',
+  twitter: 'https://twitter.com/riteshsp2000/',
+  github: 'https://github.com/riteshsp2000/slyde',
+  linkedin: 'https://www.linkedin.com/in/riteshsp2000',
+  instagram: 'https://instagram.com/riteshsp2000',
+};
