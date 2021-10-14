90b9dc0fbea9377905aad5fcb82b9e6d9604239f
Ritesh Patil <riteshsp2000@gmail.com>
2021-10-14 15:49:02 +0530

    feat: create column title component

---
+++ src/components/shared/SectionTitle.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+// Libraries
+import styled from 'styled-components';
+
+// Components
+import { Heading2 } from './typography/Heading2';
+
+const Title = styled(Heading2)`
+  color: ${({ color }) => color};
+`;
+
+const Wrapper = styled.div`
+  width: 12.5rem;
+  height: 1.5rem;
+  border-radius: 4px;
+  padding: 5px;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+const SectionTitle = ({ title, color1, color2 }) => (
+  <Wrapper color={color1}>
+    <Title color={color2}>{title}</Title>
+  </Wrapper>
+);
+
+export default SectionTitle;
