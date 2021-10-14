cc4bf904b255e098ddd6531a5dd5b4c8ad61c2b2
Ritesh Patil <riteshsp2000@gmail.com>
2021-10-15 01:36:01 +0530

    feat: create DesktopNavbar component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,18 +1,18 @@
 // Typographies
-export { default as Heading1 } from './shared/typography/Heading1';
-export { default as Heading2 } from './shared/typography/Heading2';
-export { default as Body1 } from './shared/typography/Body1';
-export { default as Body2 } from './shared/typography/Body2';
+export { default as Heading1 } from './typography/Heading1';
+export { default as Heading2 } from './typography/Heading2';
+export { default as Body1 } from './typography/Body1';
+export { default as Body2 } from './typography/Body2';
 
 // Inputs
-export { default as LargeInput } from './shared/inputs/LargeInput';
-export { default as SmallInput } from './shared/inputs/SmallInput';
-export { default as SelectInput } from './shared/inputs/Select';
+export { default as LargeInput } from './inputs/LargeInput';
+export { default as SmallInput } from './inputs/SmallInput';
+export { default as SelectInput } from './inputs/Select';
 
 // Modals
 export { default as Modal } from './Modal';
 
 // Cards
 export { default as Card } from './Card';
-export { default as SectionTitle } from './shared/SectionTitle';
+export { default as SectionTitle } from './SectionTitle';
 export { default as CardsColumn } from './CardsColumn';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import {
-  Heading1,
-  Heading2,
-  Body1,
-  Body2,
-  LargeInput,
-  SmallInput,
-  CardsColumn,
-} from '../components';
+const Home = () => <h1>Hello</h1>;
 
-const IndexPage = () => {
-  const [largeinput, setLargeInput] = useState<string | undefined>('');
-  const [smallInput, setSmallInput] = useState<string | undefined>();
-  const [showModal, setShowModal] = useState<boolean>(false);
-
-  return (
-    <div>
-      <Heading1>Title of this Page</Heading1>
-      <Body1>Description of this page if neede and what not etc etc</Body1>
-      <Heading2>Title of Card</Heading2>
-      <Body2>Description of the card</Body2>
-
-      <LargeInput
-        type='text'
-        placeholder='Title of this page'
-        value={largeinput}
-        onChange={(e) => {
-          console.log(e.target.value);
-          setLargeInput(e.target.value);
-        }}
-      />
-      <SmallInput
-        placeholder='Description of this page'
-        value={smallInput}
-        onChange={(e) => setSmallInput(e.target.value)}
-        spellCheck={false}
-      />
-
-      <CardsColumn />
-    </div>
-  );
-};
-
-export default IndexPage;
+export default Home;
